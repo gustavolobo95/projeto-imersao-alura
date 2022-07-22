92168b8f2030d43200dcaaa11c68c00cf2d045f9
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2022-07-22 16:20:20 -0300

    Logica para criação de nova imagem.

--- src/br/com/projetoImersaoAlura/controller/StickersGenerator.java
+++ src/br/com/projetoImersaoAlura/controller/StickersGenerator.java
@@ -1,5 +1,6 @@
 package br.com.projetoImersaoAlura.controller;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,6 +14,16 @@ public class StickersGenerator {
 		
 		// Leitura da Imagem: 
 		BufferedImage imagemOriginal = ImageIO.read(inputStream);
+		
+		// Cria uma nova imagem em memoria com transparencia e tamanho novo: 
+		int largura = imagemOriginal.getWidth();
+		int altura = imagemOriginal.getHeight();
+		int novaAltura = altura + 200;
+		BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+		
+		// Copiar a imagem original para nova imagem (em memoria):
+		Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+		graphics.drawImage(imagemOriginal, 0, 0, null);
 		
 	}
 	
