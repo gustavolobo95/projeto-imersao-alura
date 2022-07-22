b6a43a8fe875ea884b9a0dee55a027431b21bc41
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2022-07-22 18:01:58 -0300

    Criando os Stickers

--- src/br/com/projetoImersaoAlura/model/ConexaoAPI.java
+++ src/br/com/projetoImersaoAlura/model/ConexaoAPI.java
@@ -1,7 +1,9 @@
 package br.com.projetoImersaoAlura.model;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,23 +12,35 @@ import java.util.List;
 import java.util.Map;
 
 import br.com.projetoImersaoAlura.controller.JsonParser;
+import br.com.projetoImersaoAlura.controller.StickersGenerator;
 
 public class ConexaoAPI {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		//Tarefa: Criar uma conexão HTTP e buscar o Top 10 filmes.
+
+		// Tarefa: Criar uma conexão HTTP e buscar o Top 10 filmes.
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
 		URI endereco = URI.create(url);
 		HttpClient cliente = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
 		HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
 		String body = response.body();
-		
-		//Tarefa: Extrair apenas os dados importantes.
+
+		// Tarefa: Extrair apenas os dados importantes.
 		JsonParser parser = new JsonParser();
 		List<Map<String, String>> listaDeFilmes = parser.parse(body);
-		
+
+		// Tarefa: Criar as figurinhas.
+		StickersGenerator stickerGenerator = new StickersGenerator();
+		for (Map<String, String> filme : listaDeFilmes) {
+			String urlImagem = filme.get("image");
+			String titulo = filme.get("title");
+			InputStream inputStream = new URL(urlImagem).openStream();
+			String nomeArquivo = titulo + ".png";
+			stickerGenerator.generateSticker(inputStream, nomeArquivo);
+			System.out.println(titulo);
+
+		}
 	}
-	
+
 }
