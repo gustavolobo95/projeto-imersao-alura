d33b437473aceaabf2110c344c58880ff1707bf6
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2022-07-23 14:09:51 -0300

    Refatoração usando a nova classe HTTP.

--- src/br/com/projetoImersaoAlura/model/ConexaoAPI.java
+++ src/br/com/projetoImersaoAlura/model/ConexaoAPI.java
@@ -2,15 +2,11 @@ package br.com.projetoImersaoAlura.model;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
+import br.com.projetoImersaoAlura.controller.ClienteHTTP;
 import br.com.projetoImersaoAlura.controller.JsonParser;
 import br.com.projetoImersaoAlura.controller.StickersGenerator;
 
@@ -20,15 +16,12 @@ public class ConexaoAPI {
 
 		// Tarefa: Criar uma conexão HTTP e buscar o Top 10 filmes.
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
-		URI endereco = URI.create(url);
-		HttpClient cliente = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-		HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
-		String body = response.body();
-
+		ClienteHTTP http = new ClienteHTTP();
+		String json = http.buscarDados(url);
+		
 		// Tarefa: Extrair apenas os dados importantes.
 		JsonParser parser = new JsonParser();
-		List<Map<String, String>> listaDeFilmes = parser.parse(body);
+		List<Map<String, String>> listaDeFilmes = parser.parse(json);
 
 		// Tarefa: Criar as figurinhas.
 		StickersGenerator stickerGenerator = new StickersGenerator();
