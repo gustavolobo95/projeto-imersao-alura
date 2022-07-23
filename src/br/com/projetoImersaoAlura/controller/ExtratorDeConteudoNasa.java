5d33afe9509a26312d78788dc6b4b4b1d9505c8e
Gustavo Silva Lobo <gustavo.lobo95@gmail.com>
2022-07-23 16:04:25 -0300

    Criação de uma classe para extração da NASA.

---
+++ src/br/com/projetoImersaoAlura/controller/ExtratorDeConteudoNasa.java
@@ -0,0 +1,27 @@
+package br.com.projetoImersaoAlura.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import br.com.projetoImersaoAlura.model.Conteudo;
+
+public class ExtratorDeConteudoNasa {
+
+	public List<Conteudo> extrairConteudo(String json) {
+		// Tarefa: Extrair apenas os dados importantes.
+				JsonParser parser = new JsonParser();
+				List<Map<String, String>> listaDeConteudos = parser.parse(json);
+				List<Conteudo> conteudos = new ArrayList<Conteudo>();
+		for(Map<String, String> atributos: listaDeConteudos) {
+			
+			String titulo = atributos.get("title").replace(":", " -");
+			String url = atributos.get("url");
+			Conteudo conteudo = new Conteudo(url, titulo);
+			conteudos.add(conteudo);
+			
+		}
+		return conteudos;
+	}
+	
+}
